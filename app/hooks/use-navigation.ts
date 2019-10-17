31e09393408ef038780185c0ecccd243f6e30fc3
Addin Gama Bertaqwa <punyagama@gmail.com>
2019-10-17 22:58:22 +0800

    Navigate using hooks

---
+++ app/hooks/use-navigation.ts
@@ -0,0 +1,10 @@
+import { useContext } from 'react';
+import {
+  NavigationScreenProp,
+  NavigationRoute,
+  NavigationContext,
+} from 'react-navigation';
+
+export function useNavigation<Params>() {
+  return useContext(NavigationContext) as NavigationScreenProp<NavigationRoute,Params>;
+}
--- app/screens/sample/SampleOne.tsx
+++ app/screens/sample/SampleOne.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import { SafeAreaView } from 'react-navigation'
 import { Button, Icon } from 'react-native-elements'
+import { useNavigation } from '../../hooks'
 
-const ScreenOne = (props: any) => {
+const ScreenOne = () => {
+  const navigation = useNavigation()
   return (
     <SafeAreaView>
       <Button
         title='Click me'
         icon={<Icon name='home' />}
-        onPress={() => props.navigation.navigate('ScreenTwo')} />
+        onPress={() => navigation.navigate('ScreenTwo')} />
     </SafeAreaView>
   )
 }
+
+ScreenOne.navigationOptions = {
+  title: 'Sample Screen',
+};
 
 export default ScreenOne
